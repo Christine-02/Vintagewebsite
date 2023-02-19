b50dea4123946a868f4735233acab2a4d1896ffa
Christine-02 <christine.nj02@gmail.com>
2023-02-19 12:09:49 +0530

    inital

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Dolce Vintage</a>
+      </header>
+      <main>
+        list products
+        <img src="/logo512.png"></img>
+      </main>
+    </div>
+  );
+}
+
+export default App;
